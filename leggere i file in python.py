f8fc64531bbb62c0b5f76b3dbd30deead1617fff
Simone <simone.foi@itiscuneo.edu.it>
2025-11-17 12:11:46 +0100

    modifiche fatte in classe

--- leggere i file in python.py
+++ leggere i file in python.py
@@ -1,21 +1,24 @@
-#leggere i file in python
+def main(): #leggere i file in python
 
-file = open("./dati.csv", "r")      #oggetto file
-righe = file.readlines()            #lista di stringhe che contiene le righe del file
-print(righe)                        #stampa tutte le righe
-file.close()
+    file = open("./dati.csv", "r")      #oggetto file
+    righe = file.readlines()            #lista di stringhe che contiene le righe del file
+    print(righe)                        #stampa tutte le righe
+    file.close()
 
-print(righe[0])                     #stampa la prima riga
-titoli = righe[0].split(",")        #è una lista
-print(titoli)                       #stampa come prima ma separati da virgole
-titoli = righe[0][0:-1].split(",")  #stampa tutto tranne lo \n
-print(titoli)
+    print(righe[0])                     #stampa la prima riga
+    titoli = righe[0].split(",")        #è una lista
+    print(titoli)                       #stampa come prima ma separati da virgole
+    titoli = righe[0][0:-1].split(",")  #stampa tutto tranne lo \n
+    print(titoli)
 
-#unpacking (spacchettamento)
-titolo1, titolo2, titolo3 = righe[0][0:-1].split(",")
-print(titolo1)
+    #unpacking (spacchettamento)
+    titolo1, titolo2, titolo3 = righe[0][0:-1].split(",")
+    print(titolo1)
 
-#leggere tutte le altre righe del file e stamparle. Usare un ciclo for pythonico (senza range)
+    #leggere tutte le altre righe del file e stamparle. Usare un ciclo for pythonico (senza range)
 
-for i in righe[1:]:
-    print(i)
+    for i in righe[1:]:
+        print(i)
+
+if __name__ == "__main__":
+    main()
